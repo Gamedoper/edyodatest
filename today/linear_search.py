05feef6a771017c156927a7ccc7d6f1bddd7c70d
Gamedoper <34165580+Gamedoper@users.noreply.github.com>
2019-08-27 07:44:18 +0000

    Add files via upload

---
+++ today/linear_search.py
@@ -0,0 +1,33 @@
+#import pdb 
+import logging
+
+# list of numbers and key exists == true
+# list of num and keys does not exits == fasle
+# list in empty return ==>false
+# if key is is none =>false
+
+
+logging.basicConfig(level= logging.ERROR,filename= "/home/ubuntu/python-dev-program/test_log",format="%(asctime)s-%(levelname)s-%(message)s")
+def linear_search(n,x):
+    logging.debug("sUCC ENTERED HE FUNCTION")
+    element = []
+
+    for i in range(1,n):
+        element.append(i)
+        flag = 0
+    
+    
+    for i in element:
+        if(i==x):
+            print("number is found" + str(i))
+            flag =1
+
+        if(flag==0):
+            print("number not found")
+
+#pdb.set_trace()
+linear_search(1000,50)
+logging.error("sUCC COMPLETED HE FUNCTION")
+
+
+
---
+++ today/test_ls.py
@@ -0,0 +1,32 @@
+#list of num and key exists ==>True
+
+#list of numm and key doesnt not exit
+
+#if list is empty =>
+
+# if key is noe
+
+
+from linear_search import linear_search
+import pytest
+import logging
+c
+
+
+
+@pytest.mark.valid
+def test_valid_pos_input():
+    result = linear_search(1000,50)
+
+    assert result == True
+
+
+
+def test_valid_neg_input():
+    result = linear_search(1000,5120)
+    assert result == False
+
+@pytest.mark.valid    
+def test_invalid_input():
+    result = linear_search(0,0)
+   # assert result == False
